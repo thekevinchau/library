1dd14c2cc438e5da6312f689124ac7a6eee626bf
KEVIN CHAU <kevinch@udel.edu>
2023-11-27 09:18:19 -0500

    Added book display options for title and author

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let library = [];
+let library = [
+    new Book('Harry Potter', 'JK Rowling', 321, 'read')
+];
 
 function Book (title, author, page, read){
     this.title = title;
@@ -7,3 +9,28 @@ function Book (title, author, page, read){
     this.read = read;
 }
 
+//we want to find a way to display books currently in array.
+
+function displayBooks(array){
+    let lib = document.querySelector('.library');
+    lib.innerHTML = '';
+
+
+    array.forEach(Book => {
+        let div = document.createElement('div');
+        
+
+        let title = document.createElement('h3');
+        title.textContent = Book.title;
+        div.appendChild(title);
+
+        let author = document.createElement('p');
+        author.textContent = Book.author;
+        div.appendChild(author);
+
+
+        lib.appendChild(div);
+    })
+}
+
+displayBooks(library);
